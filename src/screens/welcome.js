539470f967286e2899a958c20dd6278a52f21222
chiragmodi1247 <mchirag1247@gmail.com>
2019-05-24 19:21:46 +0530

    removed bootstrap

--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import signin from "../components/signin";
 import SignUp from "../components/signup";
 import forget from "../components/forget";
