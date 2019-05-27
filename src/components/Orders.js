b93e0f88733b048b8ba7902a454e7fec344261b2
chiragmodi1247 <mchirag1247@gmail.com>
2019-05-27 10:45:00 +0530

    table added

---
+++ src/components/Orders.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import MaterialTableDemo from "./eventsList";
+
+function TabContainer({ children, dir }) {
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  dir: PropTypes.string.isRequired
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    flex: 1
+  }
+}));
+
+function FullWidthTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+        >
+          <Tab label="UP COMING EVENT" style={{ fontSize: 15 }} />
+          <Tab label="PAST EVENT" style={{ fontSize: 15 }} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabContainer dir={theme.direction}>
+          <MaterialTableDemo />
+        </TabContainer>
+        <TabContainer dir={theme.direction}>Item Two</TabContainer>
+      </SwipeableViews>
+    </div>
+  );
+}
+
+export default FullWidthTabs;
--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import signin from "../components/signin";
 import SignUp from "../components/signup";
 import forget from "../components/forget";
+import Dashboard from "../screens/dashboard";
 
 function welcome() {
   return (
@@ -11,6 +12,7 @@ function welcome() {
         <Route exact path="/log" component={signin} />
         <Route path="/reg" component={SignUp} />
         <Route path="/forpas" component={forget} />
+        <Route path="/dash" component={Dashboard} />
       </div>
     </Router>
   );
