7625014be79cbf9db2833ed6489955ecab124f3c
chiragmodi1247 <mchirag1247@gmail.com>
2019-05-29 01:24:07 +0530

    Pie chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -9,44 +9,55 @@ import {
 } from "recharts";
 import Title from "./Title";
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
 const data = [
-  createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
-  createData("24:00", undefined)
+  { month: "Jan", ev: 65 },
+  { month: "Feb", ev: 100 },
+  { month: "Mar", ev: 45 },
+  { month: "Apr", ev: 27 },
+  { month: "May", ev: 32 },
+  { month: "Jun", ev: 20 },
+  { month: "Jul", ev: 29 },
+  { month: "Aug", ev: 37 },
+  { month: "Sep", ev: 53 },
+  { month: "Oct", ev: 38 },
+  { month: "Nov", ev: 49 },
+  { month: "Dec", ev: 63 }
 ];
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>Activity Graph</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
           margin={{
-            top: 16,
+            top: 10,
             right: 16,
             bottom: 0,
-            left: 24
+            left: 0
           }}
         >
-          <XAxis dataKey="time" />
+          <XAxis dataKey="month">
+            <Label
+              angle={0}
+              offset={0}
+              position="insideBottom"
+              style={{ textAnchor: "middle" }}
+            >
+              Month
+            </Label>
+          </XAxis>
           <YAxis>
-            <Label angle={270} position="left" style={{ textAnchor: "middle" }}>
-              Sales ($)
+            <Label
+              angle={270}
+              position="insideLeft"
+              style={{ textAnchor: "middle" }}
+            >
+              Events
             </Label>
           </YAxis>
-          <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
+          <Line type="monotone" dataKey="ev" stroke="#556CD6" dot={true} />
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
--- src/components/Deposits.js
+++ src/components/Deposits.js
@@ -1,33 +1,61 @@
-/* eslint-disable no-script-url */
-
-import React from "react";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/styles";
-import Typography from "@material-ui/core/Typography";
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Sector, Cell } from "recharts";
 import Title from "./Title";
+const data = [
+  { name: "Small Scale", value: 400 },
+  { name: "Destination", value: 300 },
+  { name: "Mega Budget", value: 200 }
+];
 
-const useStyles = makeStyles({
-  depositContext: {
-    flex: 1
-  }
-});
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
-export default function Deposits() {
-  const classes = useStyles();
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+  name
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.35;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
   return (
-    <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
-      </Typography>
-      <div>
-        <Link color="primary" href="javascript:;">
-          View balance
-        </Link>
-      </div>
-    </React.Fragment>
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"}>
+      {`${name}`}
+    </text>
   );
+};
+
+export default class Example extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <Title>Types Of Events</Title>
+        <PieChart width={400} height={240}>
+          <Pie
+            data={data}
+            cx={260}
+            cy={110}
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={110}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </React.Fragment>
+    );
+  }
 }
